d47486188a8d5f063ee16049a099413ccf7496dc
Timothée Poisot <timothee.poisot@umontreal.ca>
2021-06-14 17:20:18 -0400

    figure changes

--- network/embed.jl
+++ network/embed.jl
@@ -40,24 +40,20 @@ for (i,s) in enumerate(species(UM))
     I[s].y = x[i,2]
 end
 
-scatter(I, M, bipartite=true, nodesize=degree(M), msc=:black, size=(1000, 1000), aspectratio=1, dpi=600, msw=0.5)
-savefig("tSNE.pdf")
-savefig("tSNE.png")
-
-ps = scatter(I, M, bipartite=true, nodesize=degree(M), msc=:black, size=(1000, 1000), dpi=600, msw=0.3)
-scatter!(ps, I, networks["Shaw"], mc=cbfp[1], bipartite=true, nodesize=degree(M), msc=:black, msw=0.3)
+ps = scatter(I, M, bipartite=true, nodesize=degree(M), msc=:grey, size=(1000, 1000), dpi=600, msw=0.3)
+scatter!(ps, I, networks["Shaw"], mc=cbfp[1], bipartite=true, nodesize=degree(M), msc=:grey, msw=0.3)
 savefig(ps, "shaw.pdf")
 
-ph = scatter(I, M, bipartite=true, nodesize=degree(M), msc=:black, size=(1000, 1000), aspectratio=1, dpi=600, msw=0.5)
-scatter!(ph, I, networks["HP3"], mc=cbfp[5], bipartite=true, nodesize=degree(M), msc=:black, msw=0.5)
+ph = scatter(I, M, bipartite=true, nodesize=degree(M), msc=:grey, size=(1000, 1000), aspectratio=1, dpi=600, msw=0.5)
+scatter!(ph, I, networks["HP3"], mc=cbfp[5], bipartite=true, nodesize=degree(M), msc=:grey, msw=0.5)
 savefig(ph, "hp3.pdf")
 
-pe = scatter(I, M, bipartite=true, nodesize=degree(M), msc=:black, size=(1000, 1000), aspectratio=1, dpi=600, msw=0.5)
-scatter!(pe, I, networks["EID2"], mc=cbfp[3], bipartite=true, nodesize=degree(M), msc=:black, msw=0.5)
+pe = scatter(I, M, bipartite=true, nodesize=degree(M), msc=:grey, size=(1000, 1000), aspectratio=1, dpi=600, msw=0.5)
+scatter!(pe, I, networks["EID2"], mc=cbfp[3], bipartite=true, nodesize=degree(M), msc=:grey, msw=0.5)
 savefig(pe, "eid2.pdf")
 
-pg = scatter(I, M, bipartite=true, nodesize=degree(M), msc=:black, size=(1000, 1000), aspectratio=1, dpi=600, msw=0.5)
-scatter!(pg, I, networks["GMPD2"], mc=cbfp[6], bipartite=true, nodesize=degree(M), msc=:black, msw=0.5)
+pg = scatter(I, M, bipartite=true, nodesize=degree(M), msc=:grey, size=(1000, 1000), aspectratio=1, dpi=600, msw=0.5)
+scatter!(pg, I, networks["GMPD2"], mc=cbfp[6], bipartite=true, nodesize=degree(M), msc=:grey, msw=0.5)
 savefig(pg, "gmpd2.pdf")
 
 title!(ps, "Shaw")
